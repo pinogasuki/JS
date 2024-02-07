7907fe6c0225fceec4d2359aecf820178bd7079a
pinogasuki <shionenene.k@icloud.com>
2024-02-07 22:17:40 +0900

    イベント発火　基礎編

--- script.js
+++ script.js
@@ -9,16 +9,21 @@ window.addEventListener('load' , function(){
 
     const pullDownButton = document.getElementById("lists");
     pullDownButton.addEventListener('mouseover' , function(){
+      pullDownButton.setAttribute("style" , "background-color:blue;")
 
         console.log("乗る");
     });
     
     pullDownButton.addEventListener('mouseout', function() {
+      pullDownButton.removeAttribute("style" , "background-color:red;")
         
         console.log("離れる");
     });
 
     pullDownButton.addEventListener('click', function() {
+        pullDownButton.setAttribute("style" , "background-color:green;")
+
+        
        
         console.log("クリック");
     });
