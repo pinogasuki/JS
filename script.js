5c79652c615582edfa0212b6adeade866b135f03
pinogasuki <shionenene.k@icloud.com>
2024-02-08 22:42:31 +0900

    条件分岐編

--- script.js
+++ script.js
@@ -1,31 +1,49 @@
 
-let lists = document.getElementsByClassName("pull-down-list");
-//console.log(lists)
+//let lists = document.getElementsByClassName("pull-down-list");
+  //console.log(lists)
 
-const pullDownButton =document.getElementById("lists");
-//console.log(pullDownButton)
+//const pullDownButton =document.getElementById("lists");
+   // console.log(pullDownButton)
 
-window.addEventListener('load' , function(){
 
-    const pullDownButton = document.getElementById("lists");
-    pullDownButton.addEventListener('mouseover' , function(){
-      pullDownButton.setAttribute("style" , "background-color:blue;")
+//window.addEventListener('load' , function(){
 
-        console.log("乗る");
-    });
+    //const pullDownButton = document.getElementById("lists");
+    //pullDownButton.addEventListener('mouseover' , function(){
+      //pullDownButton.setAttribute("style" , "background-color:blue;")
+
+        //console.log("乗る");
+    //});
     
-    pullDownButton.addEventListener('mouseout', function() {
-      pullDownButton.removeAttribute("style" , "background-color:red;")
+    //pullDownButton.addEventListener('mouseout', function() {
+      //pullDownButton.removeAttribute("style" , "background-color:red;")
         
-        console.log("離れる");
-    });
+        //console.log("離れる");
+   // });
 
-    pullDownButton.addEventListener('click', function() {
-        pullDownButton.setAttribute("style" , "background-color:green;")
+    //pullDownButton.addEventListener('click', function() {
+     // pullDownButton.setAttribute("style" , "display:block;")
 
         
-       
-        console.log("クリック");
-    });
-});
+      //  console.log("クリック");
+   // });
+//});
+
+const pullDownButton = document.getElementById("lists")
+const pullDownParents = document.getElementById("pull-down");
+pullDownButton.addEventListener('click' , function() {
+    if (pullDownParents.getAttribute("style") == "display:block;"){
+        pullDownParents.removeAttribute("style" , "display:block;")
+        console.log("非表示")
+    } else {
+        pullDownParents.setAttribute("style" , "display:block;")
+        console.log("表示")
+}
+})
+
+pullDownButton.addEventListener('mouseover' , function(){
+    this.setAttribute("style" , "background-color:blue;")
+    console.log('乗ったら青色')
+})
+
 
